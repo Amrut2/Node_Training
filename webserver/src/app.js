dd1623b5e59b41ce995754e54f3d37174035d5d7
Puyed <amrut-rajesh.puyed@capgemini.com>
2023-10-20 14:03:28 +0530

    added routing to html pages

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,18 +1,28 @@
+const path = require('path')
 const  express = require('express')
+const exp = require('constants')
 const app = express()
 
+
+
+const publicDirectorPath = path.join(__dirname, "./public");
+console.log(publicDirectorPath)
 const port = 3000
 
-app.get('' , (req, res) => {
-    res.send('hello world');
-})
+app.use(express.static(publicDirectorPath))
+
+// console.log(__dirname); // dirname is used to get path of the required folder which we want 
+// console.log(path.join(__dirname, "./public"));
+
 
 app.get('/help', (req,res)=>{
-    res.send("how can i help you?")
+    const filePath = path.join(publicDirectorPath, 'help.html');
+    res.sendFile(filePath);
 })
 
 app.get('/about', (req,res)=>{
-    res.send("about");
+    const filePath = path.join(publicDirectorPath, 'about.html')
+    res.sendFile(filePath)
 })
 
 
